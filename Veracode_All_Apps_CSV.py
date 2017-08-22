73810e7a03888660600ad9802db53de590beb027
Brian Pitta <brianpitta@gmail.com>
2017-08-22 13:09:41 -0400

    Updated to include all necessary fields with encoding to prevent errors.

--- Veracode_All_Apps_CSV.py
+++ Veracode_All_Apps_CSV.py
@@ -6,8 +6,8 @@ import os
 import multiprocessing as mp
 from functools import partial
 from lxml import etree
-import time
 import shutil
+import logging
 
 
 def cleanup(clean_type):
@@ -63,7 +63,7 @@ def get_build_list_api(api_user, api_password, app_id):
     f = open(file_name, 'w')
     f.write(r.content)
     f.close()
-    print '[*] Created ' + file_name
+    logging.info('Created ' + file_name)
     return ()
 
 
@@ -120,74 +120,107 @@ def check_mitigations(flaw, results_xml, scan_type):
     return recent_comment, recent_reviewer, recent_date
 
 
-def build_csv_fields(scan_type, flaw, app_id, tracking_id, app_name, latest_build):
-    field = {'unique_id': app_id + flaw.attrib['issueid'], 'tracking_id': tracking_id, 'app_id': app_id,
-             'app_name': app_name, 'latest_build': latest_build, 'issueid': flaw.attrib['issueid'],
-             'cweid': flaw.attrib['cweid'], 'categoryname': flaw.attrib['categoryname'],
-             'categoryid': flaw.attrib['categoryid'], 'severity': flaw.attrib['severity'],
+def build_csv_fields(scan_type, flaw, app_id, tracking_id, app_name, latest_build, recent_proposal_comment,
+                     recent_proposal_reviewer, recent_proposal_date):
+    field = {'unique_id': app_id + flaw.attrib['issueid'],
+             'tracking_id': tracking_id,
+             'app_id': app_id,
+             'app_name': app_name,
+             'latest_build': latest_build,
+             'issueid': flaw.attrib['issueid'],
+             'cweid': flaw.attrib['cweid'],
+             'categoryname': flaw.attrib['categoryname'],
+             'categoryid': flaw.attrib['categoryid'],
+             'severity': flaw.attrib['severity'],
+             'description': flaw.attrib['description'],
              'date_first_occurrence': flaw.attrib['date_first_occurrence'],
              'remediation_status': flaw.attrib['remediation_status'],
              'affects_policy_compliance': flaw.attrib['affects_policy_compliance'],
-             'mitigation_status': flaw.attrib['mitigation_status']}
+             'mitigation_status': flaw.attrib['mitigation_status'],
+             'recent_proposal_comment': recent_proposal_comment,
+             'recent_proposal_reviewer': recent_proposal_reviewer,
+             'recent_proposal_date': recent_proposal_date,
+             'flaw_attrib_text': flaw.attrib['description']}
 
     if scan_type == 'static':
         field['exploitLevel'] = flaw.attrib['exploitLevel']
+        field['sourcefile'] = flaw.attrib['sourcefile']
+        field['sourcefilepath'] = flaw.attrib['sourcefilepath']
+        field['line'] = flaw.attrib['line']
+        field['url'] = 'NA-SAST'
 
     else:
         field['exploitLevel'] = 'NA-DAST'
+        field['sourcefile'] = 'NA-DAST'
+        field['sourcefilepath'] = 'NA-DAST'
+        field['line'] = 'NA-DAST'
+        field['url'] = flaw.attrib['url']
 
-    row = (field['unique_id'], field['tracking_id'], field['app_id'], field['app_name'], field['latest_build'],
-           field['issueid'], field['cweid'], field['categoryname'], field['categoryid'], field['severity'],
+    row = (field['unique_id'],
+           field['tracking_id'],
+           field['app_id'],
+           field['app_name'].encode('utf-8'),
+           field['latest_build'],
+           field['issueid'],
+           field['cweid'],
+           field['categoryname'],
+           field['categoryid'],
+           field['severity'],
            field['exploitLevel'],
-           field['date_first_occurrence'], field['remediation_status'], field['affects_policy_compliance'],
-           field['mitigation_status'])
+           field['date_first_occurrence'],
+           field['description'].encode('utf-8'),
+           field['remediation_status'],
+           field['affects_policy_compliance'],
+           field['mitigation_status'],
+           field['recent_proposal_comment'].encode('utf-8'),
+           field['recent_proposal_reviewer'].encode('utf-8'),
+           field['recent_proposal_date'],
+           field['sourcefilepath'].encode('utf-8'),
+           field['sourcefile'].encode('utf-8'),
+           field['line'],
+           field['url'].encode('utf-8'))
 
     return row
 
 
-def create_results_xml(api_user, api_password, provided_app_list, app_id):
+def create_results_xml(api_user, api_password, app_id):
     # CHECK FOR PROVIDED LIST IN PARAMETERS
-    if provided_app_list is not None:
-        f = open(provided_app_list, "rb")
-        allowed_apps = f.read().split('\n')
-        if app_id not in allowed_apps:
-            return
-    else:
-        build_list_xml_file = 'build_xml_files' + os.path.sep + app_id + '_buildlist.xml'
-        build_list_xml = etree.parse(build_list_xml_file)
+    build_list_xml_file = 'build_xml_files' + os.path.sep + app_id + '_buildlist.xml'
+    build_list_xml = etree.parse(build_list_xml_file)
 
-        number_of_builds = len(build_list_xml.findall('{*}build'))
+    number_of_builds = len(build_list_xml.findall('{*}build'))
 
-        if number_of_builds > 0:
-            latest_build = build_list_xml.findall('{*}build')[-1].get('build_id')
+    if number_of_builds > 0:
+        latest_build = build_list_xml.findall('{*}build')[-1].get('build_id')
+        results_xml = results_api(api_user, api_password, latest_build)
+        if 'No report available' in results_xml and number_of_builds > 1:
+            latest_build = build_list_xml.findall('{*}build')[-2].get('build_id')
             results_xml = results_api(api_user, api_password, latest_build)
-            if 'No report available' in results_xml and number_of_builds > 1:
-                latest_build = build_list_xml.findall('{*}build')[-2].get('build_id')
-                results_xml = results_api(api_user, api_password, latest_build)
-            if 'No report available' in results_xml and number_of_builds > 2:
-                latest_build = build_list_xml.findall('{*}build')[-3].get('build_id')
-                results_xml = results_api(api_user, api_password, latest_build)
-        else:
-            results_xml = ''  # NEED TO SET IT TO SOMETHING FOR LOGIC CHECK
+        if 'No report available' in results_xml and number_of_builds > 2:
+            latest_build = build_list_xml.findall('{*}build')[-3].get('build_id')
+            results_xml = results_api(api_user, api_password, latest_build)
+    else:
+        results_xml = ''  # NEED TO SET IT TO SOMETHING FOR LOGIC CHECK
 
-        if 'No report available' in results_xml or number_of_builds == 0:
-            print '[*] App ID ' + app_id + ' has no valid builds. Building dummy XML'
-            results_xml = '<?xml version="1.0" encoding="UTF-8"?><error>No builds valid. Generating dummy XML for script</error>'
+    if 'No report available' in results_xml or number_of_builds == 0:
+        logging.info('App ID ' + app_id + ' has no valid builds; building dummy XML')
+        results_xml = '<?xml version="1.0" encoding="UTF-8"?><error>No builds valid. Generating dummy XML for script</error>'
 
-        file_name = 'detailed_results' + os.path.sep + app_id + '.xml'
-        f = open(file_name, 'w')
-        f.write(results_xml)
-        f.close()
-        print '[*] Created ' + file_name
+    file_name = 'detailed_results' + os.path.sep + app_id + '.xml'
+    f = open(file_name, 'w')
+    f.write(results_xml)
+    f.close()
+    logging.info('Created ' + file_name)
 
 
 def main():
-    start_time = time.time()
+    print '[*] Starting script; see veracode_all_apps_csv.log for details.'
 
     # SET UP ARGUMENTS
     parser = argparse.ArgumentParser(
         description='This script creates a CSV for flaws in the most recent build of all applications in an account. '
                     'Use optional parameters to determine to filter what flaws to include.')
+
     parser.add_argument('-u', '--username', required=True, help='API Username')
     parser.add_argument('-p', '--password', required=True, help='API Password')
     parser.add_argument('-n', '--non_policy_violating', required=False, dest='non_policy_violating',
@@ -203,9 +236,20 @@ def main():
                         help='Will export static flaws only')
     parser.add_argument('-d', '--dynamic_only', required=False, dest='dynamic_only', action='store_true',
                         help='Will export dynamic flaws only')
-    parser.add_argument('-a', '--app_list', required=False, help='Text file to limit app list')
+    parser.add_argument('-v', '--verbose', required=False, action='store_true', help='Verbose (Debug) logging')
 
     args = parser.parse_args()
+
+    # SET LOGGING
+    if args.verbose:
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.INFO
+
+    logging.basicConfig(filename='veracode_all_apps_csv.log',
+                        format='%(asctime)s - %(levelname)s - %(funcName)s - %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S%p',
+                        level=log_level)
 
     # ERROR CHECK PARAMETERS
     if args.static_only is True and args.dynamic_only is True:
@@ -216,7 +260,6 @@ def main():
     non_policy_violating_flag = args.non_policy_violating
     mitigated_flag = args.mitigated
     fixed_flag = args.fixed
-    provided_app_list = args.app_list
 
     # DELETE PREVIOUS CSV
     cleanup('start')
@@ -224,9 +267,10 @@ def main():
     # OPEN CSV FILE AND WRITE HEADERS
     with open('flaws.csv', 'wb') as f:
         wr = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
-        headers = ['unique_id', 'tracking_id', 'app_id', 'app_name', 'build_id', 'issueid', 'cweid', 'categoryname',
-                   'categoryid', 'severity', 'exploitLevel', 'date_first_occurrence',
-                   'remediation_status', 'affects_policy_compliance', 'mitigation_status']
+        headers = ['unique_id', 'tracking_id', 'app_id', 'app_name', 'latest_build', 'issueid', 'cweid',
+                   'categoryname', 'categoryid', 'severity', 'exploitLevel', 'date_first_occurrence', 'description',
+                   'remediation_status', 'affects_policy_compliance', 'mitigation_status', 'recent_proposal_comment',
+                   'recent_proposal_reviewer', 'recent_proposal_date', 'sourcefilepath', 'sourcefile', 'line', 'url']
         wr.writerow(headers)
 
         # GET THE APP LIST
@@ -246,15 +290,15 @@ def main():
         app_list = f.read().split('\n')
         f.close()
 
-        pool = mp.Pool(8)
+        pool = mp.Pool(4)
         func_build_list = partial(get_build_list_api, args.username, args.password)
         pool.map(func_build_list, app_list)
         pool.close()
         pool.join()
 
         # GET DETAILED XML FOR EACH APP
-        pool = mp.Pool(8)
-        func_detailed_report = partial(create_results_xml, args.username, args.password, provided_app_list)
+        pool = mp.Pool(4)
+        func_detailed_report = partial(create_results_xml, args.username, args.password)
         pool.map(func_detailed_report, app_list)
         pool.close()
         pool.join()
@@ -267,11 +311,11 @@ def main():
             results_xml = etree.parse(file_name)
 
             with open(file_name, 'r') as xml_file:
-                xml_string = xml_file.read().replace('\n', '')
+                xml_string = xml_file.read()
 
             if '<error>' in xml_string:
                 app_skip_check = 1
-                print '[*] Skipping App ID ' + app + ' because it has no results'
+                logging.info('Skipping App ID ' + app + ' because it has no results')
 
             # SET THE TRACKING ID
             if args.exclude_tracking_id is True:
@@ -294,67 +338,55 @@ def main():
                 dynamic_flaws = results_xml.findall('{*}severity/{*}category/{*}cwe/{*}dynamicflaws/{*}flaw')
 
                 if args.dynamic_only is not True:
-                    # FOR EACH STATIC FLAW, CHECK PARAMETERS TO SEE IF WE SHOULD SKIP
                     for flaw in static_flaws:
-                        flaw_skip_check = flaw_skip_check_func(flaw, non_policy_violating_flag, mitigated_flag,
-                                                               fixed_flag)
-                        # recent_proposed_mitigation = check_mitigations(flaw, results_xml, 'static')
-                        # recent_proposal_comment = recent_proposed_mitigation[0]
-                        # recent_proposal_reviewer = recent_proposed_mitigation[1]
-                        # recent_proposal_date = recent_proposed_mitigation[2]
+                        # FOR EACH STATIC FLAW, CHECK PARAMETERS TO SEE IF WE SHOULD SKIP
+                        flaw_skip_check = flaw_skip_check_func(flaw, non_policy_violating_flag,
+                                                               mitigated_flag, fixed_flag)
+
+                        # GET MITIGATION INFO
+                        recent_proposed_mitigation = check_mitigations(flaw, results_xml, 'static')
 
                         # WRITE DATA TO THE CSV IF WE DON'T SKIP
                         if flaw_skip_check == 0:
-                            # ENCODE FLAW DESCRIPTION TO AVOID ERRORS
-                            # flaw_attrib_text = flaw.attrib['description']
-                            # flaw_attrib_text = flaw_attrib_text.encode('utf-8')
 
-                            row = build_csv_fields('static', flaw, app, tracking_id,
-                                                   app_name,
-                                                   latest_build)
+                            row = build_csv_fields('static', flaw, app, tracking_id, app_name, latest_build,
+                                                   recent_proposed_mitigation[0], recent_proposed_mitigation[1],
+                                                   recent_proposed_mitigation[2])
 
                             wr.writerow(row)
 
                             static_app_flaw_count += 1
                             total_flaw_count += 1
 
-                    print '[*] Exported ' + str(static_app_flaw_count) + ' static flaws from ' + app_name + ' (' + str(
-                        app) + '), Build ID ' + str(
-                        latest_build)
+                    logging.info('Exported ' + str(static_app_flaw_count) + ' static flaws from ' +
+                                 app_name + ' (' + str(app) + '), Build ID ' + str(latest_build))
 
                     if args.static_only is not True:
                         # FOR EACH DYNAMIC FLAW, CHECK PARAMETERS TO SEE IF WE SHOULD SKIP
                         for flaw in dynamic_flaws:
                             flaw_skip_check = flaw_skip_check_func(flaw, non_policy_violating_flag, mitigated_flag,
                                                                    fixed_flag)
-                            # recent_proposed_mitigation = check_mitigations(flaw, results_xml, 'dynamic')
-                            # recent_proposal_comment = recent_proposed_mitigation[0]
-                            # recent_proposal_reviewer = recent_proposed_mitigation[1]
-                            # recent_proposal_date = recent_proposed_mitigation[2]
+
+                            # GET MITIGATION INFO
+                            recent_proposed_mitigation = check_mitigations(flaw, results_xml, 'dynamic')
 
                             # WRITE DATA TO THE CSV IF WE DON'T SKIP
                             if flaw_skip_check == 0:
-                                # ENCODE FLAW DESCRIPTION TO AVOID ERRORS
-                                # flaw_attrib_text = flaw.attrib['description']
-                                # flaw_attrib_text = flaw_attrib_text.encode('utf-8')
 
-                                row = build_csv_fields('dynamic', flaw, app, tracking_id,
-                                                       app_name,
-                                                       latest_build)
+                                row = build_csv_fields('dynamic', flaw, app, tracking_id, app_name, latest_build,
+                                                       recent_proposed_mitigation[0], recent_proposed_mitigation[1],
+                                                       recent_proposed_mitigation[2])
                                 wr.writerow(row)
 
                                 dynamic_app_flaw_count += 1
                                 total_flaw_count += 1
 
-                        print '[*] Exported ' + str(
-                            dynamic_app_flaw_count) + ' dynamic flaws from ' + app_name + ' (' + app + '), Build ID ' + str(
-                            latest_build)
+                        logging.info('Exported ' + str(dynamic_app_flaw_count) + ' dynamic flaws from ' +
+                                     app_name + ' (' + app + '), Build ID ' + str(latest_build))
 
-    print '[*] Exported ' + str(total_flaw_count) + ' total flaws'
+    logging.info('Exported ' + str(total_flaw_count) + ' total flaws')
 
     cleanup('end')
-
-    print('--- %s seconds ---' % (time.time() - start_time))
 
 
 if __name__ == '__main__':
